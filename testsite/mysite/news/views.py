22e9ee85500c0121126ef4ed3049fc76d1badfc4
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2021-04-12 13:49:24 +0700

    Added class ListView. Part 1.

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
+
 from .models import *
 from .forms import *
 
 # Create your views here.
+
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_list.html'
+    context_object_name = 'news'
+    # extra_context = {'title': 'Main'}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Main page'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
 
 def index(request):
     news = News.objects.all()
