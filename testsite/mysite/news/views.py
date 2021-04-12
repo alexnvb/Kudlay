3b4cba0e961087d90567b2bafd45e718d3e1065e
alexnvb <alexgridin@mail.ru>
2021-04-12 12:29:55 +0700

    clone

---
+++ testsite/mysite/news/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import *
+from .forms import *
+
+# Create your views here.
+
+def index(request):
+    news = News.objects.all()
+
+    context = {
+        'news': news,
+        'title': 'News List',
+    }
+    return render(request, 'news/index.html', context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+
+    context = {
+        'news': news,
+        'category': category,
+    }
+    return render(request, 'news/index.html', context)
+
+def view_news(request, news_id):
+  # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+
+    context = {
+        'news_item': news_item
+    }
+    return render(request, 'news/view_news.html', context)
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            #news = News.objects.create(**form.cleaned_data)
+
+            news = form.save()
+            return redirect(news)
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
