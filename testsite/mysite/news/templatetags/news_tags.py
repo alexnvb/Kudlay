126dd4db8fa86d2c2a0cdfe7bc80c2e0135d066f
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2021-04-15 14:58:35 +0700

    Lesson 37. Added Badge in Categories sidebar.

--- testsite/mysite/news/templatetags/news_tags.py
+++ testsite/mysite/news/templatetags/news_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from news.models import *
+from django.db.models import Count
 
 register = template.Library()
 
@@ -9,5 +10,6 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello'):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {"categories": categories, "arg1": arg1}
