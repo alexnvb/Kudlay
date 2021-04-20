3ecec9cabdedad0b7bd942a0193a79c8c42dbe01
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2021-04-20 12:30:17 +0700

    20.04.21 added something!

--- testsite/mysite/news/urls.py
+++ testsite/mysite/news/urls.py
@@ -20,6 +20,8 @@ from .views import *
 
 urlpatterns = [
 
+
+
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
