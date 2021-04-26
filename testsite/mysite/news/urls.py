67af4b15a430e758cf0d5eef7546a71037bec563
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2021-04-26 12:22:05 +0700

    26.04.21 added merge new_branch!

--- testsite/mysite/news/urls.py
+++ testsite/mysite/news/urls.py
@@ -19,6 +19,7 @@ from .views import *
 
 
 urlpatterns = [
+
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
